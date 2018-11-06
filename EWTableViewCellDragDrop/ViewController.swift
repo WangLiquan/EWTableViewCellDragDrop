44c3408ca8a2540e9d52ff62e163c3e524892e1d
wangliquan <wang197200@163.com>
2018-11-06 10:43:09 +0800

    更新iOS11以下版本拖拽实现

--- EWTableViewCellDragDrop/ViewController.swift
+++ EWTableViewCellDragDrop/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 var colorArr = [UIColor.gray,UIColor.green,UIColor.yellow,UIColor.blue,UIColor.brown]
 
 class ViewController: UIViewController {
-
+    private var tableView:UITableView!
     var modelArray: [EWColorModel] = {
         var array = [EWColorModel]()
         for i in 0..<5{
@@ -22,7 +22,6 @@ class ViewController: UIViewController {
         }
         return array
     }()
-
     var secondModelArray: [EWColorModel] = {
         var array = [EWColorModel]()
         for i in 0..<5{
@@ -34,8 +33,12 @@ class ViewController: UIViewController {
         return array
     }()
 
+    private var touchPoints: [CGPoint] = []
+    private var sourceIndexPath: IndexPath?
+    private var cellImageView = UIImageView()
+    private var gestureRecognizer: UILongPressGestureRecognizer?
+    private var currentCell:EWDragTableViewCell!
 
-    private var tableView:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +52,7 @@ class ViewController: UIViewController {
         tableView.backgroundColor = .white
         tableView.separatorStyle = .none
         tableView.delegate = self
+
         tableView.dataSource = self
         tableView.register(EWDragTableViewCell.self, forCellReuseIdentifier: EWDragTableViewCell.identifier)
         self.view.addSubview(tableView)
@@ -61,6 +65,123 @@ class ViewController: UIViewController {
         }else{
             self.automaticallyAdjustsScrollViewInsets = false
         }
+    }
+
+    private func dragCell(cell:UITableViewCell?){
+        if #available(iOS 11.0, *)  {
+            cell?.userInteractionEnabledWhileDragging = true
+        }else {
+            let pan = UILongPressGestureRecognizer(target: self, action: #selector(self.longPressGesture))
+            cell?.addGestureRecognizer(pan)
+        }
+    }
+}
+//MARK: - UITableView ios11以下版本拖拽
+extension ViewController{
+    @objc func longPressGesture(_ recognise: UILongPressGestureRecognizer) {
+        let currentPoint: CGPoint = recognise.location(in: tableView)
+        let currentIndexPath = tableView.indexPathForRow(at: currentPoint)
+        guard let indexPath = currentIndexPath else {
+            initCellImageView()
+            return
+        }
+        if indexPath.section == 0 {
+            guard indexPath.row < self.modelArray.count else {
+                initCellImageView()
+                return
+            }
+        } else {
+            guard indexPath.row < self.secondModelArray.count else {
+                initCellImageView()
+                return
+            }
+        }
+        switch recognise.state {
+        case .began:
+            longPressGestureBegan(recognise)
+            break
+        case .changed:
+            longPressGestureChanged(recognise)
+            break
+        default:
+            self.touchPoints.removeAll()
+            if let cell = tableView.cellForRow(at: sourceIndexPath! ){
+                cell.isHidden = false
+            }
+            initCellImageView()
+            break
+        }
+    }
+    private func longPressGestureBegan(_ recognise: UILongPressGestureRecognizer) {
+        self.gestureRecognizer = recognise
+        let currentPoint: CGPoint = recognise.location(in: tableView)
+        let currentIndexPath = tableView.indexPathForRow(at: currentPoint)
+        if (currentIndexPath != nil) {
+            sourceIndexPath = currentIndexPath
+            currentCell = tableView.cellForRow(at: currentIndexPath! ) as? EWDragTableViewCell
+            cellImageView = getImageView(currentCell)
+            cellImageView.frame = currentCell.frame
+            tableView.addSubview(cellImageView)
+            self.currentCell.isHidden = true
+        }
+    }
+    private func longPressGestureChanged(_ recognise: UILongPressGestureRecognizer) {
+        let selectedPoint: CGPoint = recognise.location(in: tableView)
+        var selectedIndexPath = tableView.indexPathForRow(at: selectedPoint)
+        self.touchPoints.append(selectedPoint)
+        if self.touchPoints.count > 2 {
+            self.touchPoints.remove(at: 0)
+        }
+        var center = cellImageView.center
+        center.y = selectedPoint.y
+        // 快照随触摸点x值改变量移动
+        let Ppoint = self.touchPoints.first
+        let Npoint = self.touchPoints.last
+        let moveX = Npoint!.x - Ppoint!.x
+        center.x += moveX
+        cellImageView.center = center
+        if ((selectedIndexPath != nil) && selectedIndexPath != sourceIndexPath) {
+            tableView.beginUpdates()
+            var cellmode: EWColorModel
+            if sourceIndexPath!.section == 0 {
+                cellmode = modelArray[sourceIndexPath!.row]
+                self.modelArray.remove(at: sourceIndexPath!.row)
+            } else {
+                cellmode = secondModelArray[sourceIndexPath!.row]
+                self.secondModelArray.remove(at: sourceIndexPath!.row)
+            }
+            objc_sync_enter(self)
+            if selectedIndexPath?.section == 0 {
+                if selectedIndexPath!.row < self.modelArray.count {
+                    self.modelArray.insert(cellmode, at: selectedIndexPath!.row)
+                }else {
+                    self.modelArray.append(cellmode)
+                }
+            } else {
+                if selectedIndexPath!.row < self.secondModelArray.count {
+                    self.secondModelArray.insert(cellmode, at: selectedIndexPath!.row)
+                }else {
+                    self.secondModelArray.append(cellmode)
+                }
+            }
+            objc_sync_exit(self)
+            self.tableView.moveRow(at: sourceIndexPath!, to: selectedIndexPath!)
+            tableView.endUpdates()
+            sourceIndexPath = selectedIndexPath
+        }
+    }
+    private func initCellImageView() {
+        self.cellImageView.removeFromSuperview()
+        tableView.reloadData()
+    }
+    // 截图
+    private func getImageView(_ cell: UITableViewCell) -> UIImageView {
+        UIGraphicsBeginImageContextWithOptions(cell.bounds.size, false, 0)
+        cell.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        let imageView = UIImageView(image: image)
+        return imageView
     }
 }
 
@@ -83,6 +204,12 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
             model = modelArray[indexPath.row]
         }else{
             model = secondModelArray[indexPath.row]
+        }
+        if #available(iOS 11.0, *)  {
+            cell.userInteractionEnabledWhileDragging = true
+        }else {
+            let pan = UILongPressGestureRecognizer(target: self, action: #selector(self.longPressGesture))
+            cell.addGestureRecognizer(pan)
         }
         cell.textLabel?.text = model.title
         cell.backgroundColor = model.color
